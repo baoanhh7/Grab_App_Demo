5abe00fc88f2332916c7d2538191e39d5cbc6dfc
Bao Anh <namle0442@gmail.com>
2024-04-07 03:37:29 +0700

    commit 7/4 3:36 AM

--- app/src/main/java/com/example/grab_demo/MainActivity.java
+++ app/src/main/java/com/example/grab_demo/MainActivity.java
@@ -2,11 +2,7 @@ package com.example.grab_demo;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
