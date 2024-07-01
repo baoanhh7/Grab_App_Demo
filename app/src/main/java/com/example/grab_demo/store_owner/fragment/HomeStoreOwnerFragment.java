686d3fe95c02ae2f09bd0d5e778df7b4da75a8bc
Dennis040 <22dh111529@st.huflit.edu.vn>
2024-07-01 23:14:43 +0700

    update storeowner 1/7

---
+++ app/src/main/java/com/example/grab_demo/store_owner/fragment/HomeStoreOwnerFragment.java
@@ -0,0 +1,21 @@
+package com.example.grab_demo.store_owner.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.grab_demo.R;
+
+
+public class HomeStoreOwnerFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home_store_owner, container, false);
+    }
+}
