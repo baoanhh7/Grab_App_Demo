045cdf12c0679aab7089bdd73be99fa61bfb7340
Dennis040 <22dh111529@st.huflit.edu.vn>
2024-07-06 09:23:42 +0700

    update storeowner 6/7 9H23

---
+++ app/src/main/java/com/example/grab_demo/store_owner/adapter/ImageSliderAdapter_Home.java
@@ -0,0 +1,48 @@
+package com.example.grab_demo.store_owner.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.example.grab_demo.R;
+
+public class ImageSliderAdapter_Home extends PagerAdapter {
+    private Context context;
+    private int[] images;
+    public ImageSliderAdapter_Home(Context context, int[] images) {
+        this.context = context;
+        this.images = images;
+    }
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.item_image_slider_homestoreowner, container, false);
+
+        ImageView imageView = view.findViewById(R.id.img_slide);
+        imageView.setImageResource(images[position]);
+
+        container.addView(view);
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View) object);
+    }
+}
--- app/src/main/java/com/example/grab_demo/store_owner/fragment/HomeStoreOwnerFragment.java
+++ app/src/main/java/com/example/grab_demo/store_owner/fragment/HomeStoreOwnerFragment.java
@@ -1,20 +1,66 @@
 package com.example.grab_demo.store_owner.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.grab_demo.R;
+import com.example.grab_demo.store_owner.adapter.ImageSliderAdapter_Home;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class HomeStoreOwnerFragment extends Fragment {
+
+    private int currentPage = 0;
+    private Timer timer;
+    private ViewPager viewPager;
+    private View view;
+    private int[] images = { R.drawable.voucher2, R.drawable.voucher3,R.drawable.voucher4};
+
     @Override
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_store_owner, container, false);
+        view = inflater.inflate(R.layout.fragment_home_store_owner, container, false);
+        addControls();
+        addEvents();
+        startAutoSlide();
+        return view;
+    }
+    private void startAutoSlide() {
+        final Handler handler = new Handler();
+        final Runnable update = new Runnable() {
+            public void run() {
+                if (currentPage == images.length) {
+                    currentPage = 0;
+                }
+                viewPager.setCurrentItem(currentPage++, true);
+            }
+        };
+
+        timer = new Timer(); // This will create a new Thread
+        timer.schedule(new TimerTask() { // task to be scheduled
+            @Override
+            public void run() {
+                handler.post(update);
+            }
+        }, 1000, 3000); // Delay 1 sec, repeat every 3 sec
+    }
+    private void addEvents() {
+    }
+
+    private void addControls() {
+        viewPager = view.findViewById(R.id.viewPager_HomeStoreOwner);
+        ImageSliderAdapter_Home imageSliderAdapterHome = new ImageSliderAdapter_Home(getContext(), images);
+        viewPager.setAdapter(imageSliderAdapterHome);
     }
 }
