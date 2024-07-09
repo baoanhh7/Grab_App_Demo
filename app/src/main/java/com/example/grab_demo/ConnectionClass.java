d2fb8f54cbd1c7140b92005e6bd27c144baa66a3
Dennis040 <22dh111529@st.huflit.edu.vn>
2024-07-09 13:13:00 +0700

    update storeowner 9/7 1h23

--- app/src/main/java/com/example/grab_demo/ConnectionClass.java
+++ app/src/main/java/com/example/grab_demo/ConnectionClass.java
@@ -9,14 +9,13 @@ import java.sql.DriverManager;
 
 public class ConnectionClass {
     Connection connection;
-    private static final String DB_URL = "jdbc:jtds:sqlserver://192.168.1.20:1433;databaseName=FoodOrderingSystem";
+    private static final String DB_URL = "jdbc:jtds:sqlserver://172.31.117.71:1433;databaseName=FoodOrderingSystem";
     private static final String USER = "sa";
     private static final String PASS = "1";
     @SuppressLint("NewApi")
     public Connection conClass(){
         StrictMode.ThreadPolicy a = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(a);
-        String ConnectURL= null;
         // Tải driver JDBC
         try {
             Class.forName("net.sourceforge.jtds.jdbc.Driver");
