aea9fd6dd24329f57dff7e64a693b575fed92dc9
Dennis040 <22dh111529@st.huflit.edu.vn>
2024-07-23 15:32:31 +0700

    update storeowner 23/7

--- app/src/main/java/com/example/grab_demo/store_owner/activity/ListQuanActivity.java
+++ app/src/main/java/com/example/grab_demo/store_owner/activity/ListQuanActivity.java
@@ -70,7 +70,7 @@ public class ListQuanActivity extends AppCompatActivity {
         connection = sql.conClass();
         if (connection != null) {
             try {
-                query = "SELECT store_name, open_store, image FROM Stores WHERE owner_id = " + userId + " AND status = 'closed' OR status = 'closed' ";
+                query = "SELECT store_name, open_store, image FROM Stores WHERE owner_id = " + userId + " AND status = 'closed' OR status = 'opened' ";
                 smt = connection.createStatement();
                 resultSet = smt.executeQuery(query);
                 while (resultSet.next()) {
